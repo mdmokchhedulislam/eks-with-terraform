5aefde2cb4b99d8cdb058eb552f41221c64633d6
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-07-16 07:35:29 +0600

    eks worker node creation successfully and connect with cluster node

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -38,3 +38,75 @@ resource "aws_eks_cluster" "controller" {
     aws_iam_role_policy_attachment.AmazonEKSClusterPolicy
   ]
 }
+
+
+
+
+
+
+
+
+
+
+
+resource "aws_iam_role" "node_role" {
+  name = var.cluster_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy "
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly  "
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+
+
+
+
+
+
+resource "aws_eks_node_group" "worker_node" {
+  cluster_name    = aws_eks_cluster.controller.name
+  node_group_name = "worker"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly  ,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy  ,
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy ,
+  ]
+}
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -6,6 +6,14 @@ variable "cluster_role_name" {
   
 }
 
+variable "node_role_name" {
+    description = "eks cluster role name"
+    type = string
+    default = "eks_node_role"
+  
+}
+
+
 
 variable "subnet_ids" {
   description = "List of subnet IDs for the EKS cluster"
